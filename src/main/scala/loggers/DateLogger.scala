35727dc5b7cc09e0853f3ec7b0ceebbb1cd3b6b7
Tristan Timblin <ttimblin@Tristans-MacBook-Pro-2.local>
2025-03-23 21:01:11 -0700

    LOGGER

--- src/main/scala/loggers/DateLogger.scala
+++ src/main/scala/loggers/DateLogger.scala
@@ -2,6 +2,7 @@ import java.time.LocalDate
 
 object DateLogger extends Logger {
   override def name: String = "date"
+
   override def generateLogEntry(params: Map[String, String]): String =
     s"${LocalDate.now()},DATE"
 }
